dbeb8bc01e4df8b79db4a81e5251310fb96baa33
Erick Pena <penaerick23@gmail.com>
2026-07-16 16:28:25 -0600

    Array of Struct Struct

--- Nested Functions/Struct.sql
+++ Nested Functions/Struct.sql
@@ -27,3 +27,29 @@ SELECT
         type := types
     )
 FROM skill_table;
+
+--Array of Struct--
+SELECT [
+{ skill: 'python', type: 'programming'}, 
+{ skill: 'sql', type: 'query_language'}
+
+] AS skills_array_of_structures;
+
+
+WITH skill_table AS (
+SELECT 'python' AS skills, 'programming' AS types
+UNION ALL 
+SELECT 'sql', 'query_language'
+UNION ALL 
+SELECT 'r', 'programming'
+), skills_array_struct AS (
+SELECT 
+    ARRAY_AGG(
+        STRUCT_PACK(
+        skill := skills,
+        type := types
+    )
+) array_struct
+FROM skill_table
+)
+SELECT * FROM skills_array_struct;
